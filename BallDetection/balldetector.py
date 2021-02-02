fddbfd284ccbea3a2b47409e94d268c1a4dcd769
Robert Luttrell <robert.luttrell2@gmail.com>
2021-02-02 13:13:12 -0800

    Refactored im_dict to instance variable

--- BallDetection/balldetector.py
+++ BallDetection/balldetector.py
@@ -9,28 +9,27 @@ class BallDetector:
         self.hsv_thresh_lower = hsv_thresh_lower
         self.hsv_thresh_upper = hsv_thresh_upper
         self.cap = cap
+        self.im_dict = {}
 
     def loop(self):
         while True:
-            im_dict = {}
+            _, self.im_dict["frame"] = self.cap.read()
 
-            _, im_dict["frame"] = self.cap.read()
-
-            hsv = cv2.cvtColor(im_dict["frame"], cv2.COLOR_BGR2HSV)
+            hsv = cv2.cvtColor(self.im_dict["frame"], cv2.COLOR_BGR2HSV)
 
             lower_yellow = np.array([23, 10, 187], dtype=np.uint8)
             upper_yellow = np.array([89, 190, 255], dtype=np.uint8)
 
-            im_dict["mask"] = cv2.inRange(hsv, lower_yellow, upper_yellow)
+            self.im_dict["mask"] = cv2.inRange(hsv, lower_yellow, upper_yellow)
             # im_dict["frame_gray"] = cv2.cvtColor(im_dict["frame"], cv2.COLOR_BGR2GRAY)
-            im_dict["masked"] = cv2.bitwise_and(im_dict["frame"], im_dict["frame"], mask=im_dict["mask"])
-            im_dict["gray"] = cv2.cvtColor(im_dict["masked"], cv2.COLOR_BGR2GRAY)
-            im_dict["gray_blurred"] = cv2.blur(im_dict["gray"], (3, 3))
+            self.im_dict["masked"] = cv2.bitwise_and(self.im_dict["frame"], self.im_dict["frame"], mask=self.im_dict["mask"])
+            self.im_dict["gray"] = cv2.cvtColor(self.im_dict["masked"], cv2.COLOR_BGR2GRAY)
+            self.im_dict["gray_blurred"] = cv2.blur(self.im_dict["gray"], (3, 3))
 
-            im_dict["output"] = im_dict["frame"].copy()
-            im_dict["canny"] = cv2.Canny(im_dict["frame"], 100, 200)
+            self.im_dict["output"] = self.im_dict["frame"].copy()
+            self.im_dict["canny"] = cv2.Canny(self.im_dict["frame"], 100, 200)
 
-            detected_circles = cv2.HoughCircles(im_dict["canny"],
+            detected_circles = cv2.HoughCircles(self.im_dict["canny"],
                                                 cv2.HOUGH_GRADIENT, 1, 20, param1=50,
                                                 param2=20, minRadius=10, maxRadius=30)
 
@@ -50,17 +49,17 @@ class BallDetector:
                         min_b = b
 
                     # Draw the circumference of the circle.
-                    cv2.circle(im_dict["output"], (a, b), r, (0, 255, 0), 2)
+                    cv2.circle(self.im_dict["output"], (a, b), r, (0, 255, 0), 2)
 
                     # Draw a small circle (of radius 1) to show the center.
-                    cv2.circle(im_dict["output"], (a, b), 1, (0, 0, 255), 3)
+                    cv2.circle(self.im_dict["output"], (a, b), 1, (0, 0, 255), 3)
                     i += 1
                 pt_min = detected_circles[0, :][min_index]
                 a_min, b_min, r_min = pt_min[0], pt_min[1], pt_min[2]
-                cv2.circle(im_dict["output"], (a_min, b_min), r, (255, 0, 0), 5)
+                cv2.circle(self.im_dict["output"], (a_min, b_min), r, (255, 0, 0), 5)
 
-            for im_name in im_dict.keys():
-                im_resized = detection_utils.resize_with_aspect_ratio(im_dict.get(im_name), width=500)
+            for im_name in self.im_dict.keys():
+                im_resized = detection_utils.resize_with_aspect_ratio(self.im_dict.get(im_name), width=500)
                 cv2.imshow(im_name, im_resized)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
